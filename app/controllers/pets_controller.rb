efc7de2ddacf2b738e40586321e5463085c38dfd
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-18 23:27:45 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,24 +1,3 @@
-Skip to content
-
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- @torianne02 Sign out
-0
-0 896 RickySauce/sinatra-complex-forms-associations-v-000
-forked from learn-co-students/sinatra-complex-forms-associations-v-000
- Code  Pull requests 0  Projects 0  Insights
-sinatra-complex-forms-associations-v-000/app/controllers/pets_controller.rb
-a5603a6  9 days ago
-@RickySauce RickySauce Done.
-@SophieDeBenedetto @RickySauce
-
-44 lines (35 sloc)  838 Bytes
-
-
 class PetsController < ApplicationController
 
   get '/pets' do
